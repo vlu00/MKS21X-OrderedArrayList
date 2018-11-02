b9b7686016188dc74868b38799edb72b2f9856b3
vlu00 <vlu00@stuy.edu>
2018-11-02 10:14:55 -0400

    made some progress on orderArray(helper method)

---
+++ OrderedArrayList.java
@@ -0,0 +1,22 @@
+public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
+  public OrderedArrayList() {
+  }
+
+  public OrderedArayListA(int startingCapacity) {
+    super(startingCapacity);
+  }
+
+  public orderArray(ArrayList<T> a) {
+    int value = 0;
+    for (int i = 1; i < a.size(); i++) {
+      for (int n = 1; i < a.size(); n++) {
+        if (a.get(i).compareTo(a.get(i-1)) <  0) {
+          set
+      }
+
+
+      }
+    }
+  }
+
+}
