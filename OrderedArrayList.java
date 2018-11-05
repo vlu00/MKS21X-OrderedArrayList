c3ba2d6c8ed350fa087e680e73e2bee52a09241b
vlu00 <vlu00@stuy.edu>
2018-11-04 23:27:13 -0500

    tried Timothy's driver and found problem with add

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -16,14 +16,19 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public void add(int index, T element) {
     int x = 0;
     for (int i = 0; i < this.size(); i++) {
-      if (i+1 == this.size() &&  element.compareTo(this.get(i)) > -1) {
+      if (i == 0 && element.compareTo(this.get(i)) < 1) {
+        x = 0;
+        i = this.size();
+      }
+      else if (i+1 == this.size() &&  element.compareTo(this.get(i)) > -1) {
         x = i+1;
       }
       else if (i+1 == this.size() &&  element.compareTo(this.get(i)) < 0) {
         x = i;
       }
       else if (element.compareTo(this.get(i)) > -1 &&  element.compareTo(this.get(i+1)) < 1 ) {
-        x = i;
+        x = i+1;
+        i = this.size();
       }
     }
     super.add(x, element);
