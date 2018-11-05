e7172fcc2a6618a408431038c032b7a70df625ab
vlu00 <vlu00@stuy.edu>
2018-11-04 22:48:06 -0500

    wrote and tested boolean add

---
+++ MyDriver.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class MyDriver{
+  public static void main(String[] args) {
+  OrderedArrayList<Integer> list = new OrderedArrayList<>();
+  list.add(2);
+  list.add(1);
+  list.add(3);
+  list.add(5); 
+  System.out.println(list);
+}
+}
--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,22 +1,38 @@
+import java.util.ArrayList;
+
 public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
   public OrderedArrayList() {
   }
 
-  public OrderedArayListA(int startingCapacity) {
+  public OrderedArrayList(int startingCapacity) {
     super(startingCapacity);
   }
 
-  public orderArray(ArrayList<T> a) {
-    int value = 0;
-    for (int i = 1; i < a.size(); i++) {
-      for (int n = 1; i < a.size(); n++) {
-        if (a.get(i).compareTo(a.get(i-1)) <  0) {
-          set
+  public boolean add(T element) {
+    int index = 0;
+    for (int i = 0; i < this.size(); i++) {
+      if (i+1 == this.size() &&  element.compareTo(this.get(i)) > -1) {
+        index = i+1;
       }
-
-
+      else if (i+1 == this.size() &&  element.compareTo(this.get(i)) < 0) {
+        index = i;
+      }
+      else if (element.compareTo(this.get(i)) > -1 &&  element.compareTo(this.get(i+1)) < 1 ) {
+        index = i;
       }
     }
+    super.add(index, element);
+    return true;
   }
+
+  /*public void add(int index, T element) {
+    add(element);
+  }
+
+  public T set(int index, T element) {
+    add(element);
+  }
+
+*/
 
 }
